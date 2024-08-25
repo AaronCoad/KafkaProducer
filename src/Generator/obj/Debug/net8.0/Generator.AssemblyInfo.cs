b2c7e6a2311c51eba18539802eb400fc07e03332
Aaron Coad <coadaaron@gmail.com>
2024-08-25 13:24:38 +1000

    Remove bin and obj

--- src/Generator/obj/Debug/net8.0/Generator.AssemblyInfo.cs
+++ src/Generator/obj/Debug/net8.0/Generator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Generator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c9704bcd50397d6c3c276e95eec57d5a0f3ea24")]
 [assembly: System.Reflection.AssemblyProductAttribute("Generator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Generator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
